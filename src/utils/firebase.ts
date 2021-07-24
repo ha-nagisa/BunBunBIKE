31ca89bd6889e1755024120f66f08636136c3bac
ha-nagisa <nagisa18222@icloud.com>
2021-07-24 23:26:24 +0900

    typescriptへ移行の途中

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,35 +2,46 @@ import firebaseApp from 'firebase';
 import { firebase, FieldValue } from '../libs/firebase';
 
 interface responceUserData {
-  bikeImageUrl?: string;
-  carModel?: string;
+  bikeImageUrl: string;
+  carModel: string;
   dateCreated: number;
-  emailAddress?: string;
-  followers?: string[];
-  following?: string[];
+  emailAddress: string;
+  followers: string[];
+  following: string[];
   likes: string[];
-  maker?: string;
+  maker: string;
   userId: string;
-  username?: string;
+  username: string;
   docId?: string;
 }
 
 interface responcePhotoData {
-  Maker?: string;
-  carModel?: string;
-  category?: string;
+  Maker: string;
+  carModel: string;
+  category: string;
   comments: string[];
   dateCreated: number;
-  description?: string;
-  imageSrc?: string;
+  description: string;
+  imageSrc: string;
   likes: string[];
-  title?: string;
-  userId?: string;
-  workHours?: string;
-  workMoney?: string;
+  title: string;
+  userId: string;
+  workHours: string;
+  workMoney: string;
   docId?: string;
 }
 
+interface responsePhotoUserInfo {
+  userLikedPhoto: boolean;
+  username: string;
+}
+
+interface lastDocType {
+  lastdoc: firebaseApp.firestore.QueryDocumentSnapshot<firebaseApp.firestore.DocumentData> | undefined;
+}
+
+type responceUserDataWithUserInfo = responcePhotoData | responsePhotoUserInfo;
+type responceUserDataWithUserInfoLastDoc = responceUserDataWithUserInfo | lastDocType;
 type responceBothData = responceUserData | responcePhotoData;
 
 // ユーザーネームを持ったユーザーが存在するかをチェック
@@ -114,7 +125,7 @@ export async function getDocumentByArrays(
 export async function getDocumentByArraysIn(
   arr: string[],
   getQuery: firebaseApp.firestore.Query<firebaseApp.firestore.DocumentData>
-): Promise<responceUserData[] | responcePhotoData[]> {
+): Promise<responceBothData[]> {
   // eslint-disable-next-line consistent-return
   return new Promise((resolve) => {
     if (!arr) return resolve([]);
@@ -237,7 +248,7 @@ export async function updateFollowedUserFollowers(profileDocId: string, loggedIn
 }
 
 // ログインしているユーザーがフォローしている人のポストを取得
-export async function getPhotos(userId: string, following: string[]): Promise<responceUserData[]> {
+export async function getPhotos(userId: string, following: string[]): Promise<responceUserDataWithUserInfo[]> {
   if (following && following.length > 0) {
     const collectionPath = firebase.firestore().collection('photos');
     const getQuery = (batch: string[]) => collectionPath.where('userId', 'in', batch);
@@ -249,28 +260,28 @@ export async function getPhotos(userId: string, following: string[]): Promise<re
         if (photo.likes.includes(userId)) {
           userLikedPhoto = true;
         }
-        // photo.userId = 2
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         const user = await getUserByUserId(photo.userId);
-        // raphael
         const { username } = user[0];
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         return { username, ...photo, userLikedPhoto };
       })
     );
 
-    return { photosWithUserDetails };
+    return photosWithUserDetails;
   }
   return [];
 }
 
 // ログインしているユーザーがお気に入りしているポストを取得
-export async function getPhotosFavorite(userId, likes) {
-  let likesPhotos;
+export async function getPhotosFavorite(userId: string, likes: string[]): Promise<Promise<responceUserDataWithUserInfo[]>> {
+  let likesPhotos: responcePhotoData[];
 
   await Promise.all(likes.map((docId) => firebase.firestore().collection('photos').doc(docId).get())).then((docs) => {
     likesPhotos = docs
       .filter((doc) => !!doc.data())
       .map((doc) => ({
-        ...doc.data(),
+        ...(doc.data() as responcePhotoData),
         docId: doc.id,
       }));
   });
@@ -293,7 +304,7 @@ export async function getPhotosFavorite(userId, likes) {
 }
 
 // すべてのポストを取得
-export async function getPhotosAll(userId, latestDoc) {
+export async function getPhotosAll(userId: string, latestDoc: string): Promise<responceUserDataWithUserInfoLastDoc[]> {
   let result;
 
   if (latestDoc) {
@@ -311,7 +322,7 @@ export async function getPhotosAll(userId, latestDoc) {
   const lastDoc = result ? result.docs[result.docs.length - 1] : undefined;
 
   const userFollowedPhotos = result.docs.map((photo) => ({
-    ...photo.data(),
+    ...(photo.data() as responcePhotoData),
     docId: photo.id,
   }));
 
@@ -329,16 +340,16 @@ export async function getPhotosAll(userId, latestDoc) {
     })
   );
 
-  return { photosWithUserDetails, lastDoc };
+  return { ...photosWithUserDetails, ...lastDoc };
 }
 
 // ユーザーIDによってポストを取得
-export async function getUserPhotosByUserId(userId) {
+export async function getUserPhotosByUserId(userId: string): Promise<responceUserDataWithUserInfoLastDoc[] | null> {
   if (userId) {
     const result = await firebase.firestore().collection('photos').where('userId', '==', userId).get();
 
     const userPhotos = result.docs.map((photo) => ({
-      ...photo.data(),
+      ...(photo.data() as responcePhotoData),
       docId: photo.id,
     }));
 
@@ -362,15 +373,15 @@ export async function getUserPhotosByUserId(userId) {
 }
 
 // ユーザーをフォローしているかをチェック
-export async function isUserFollowingProfile(loggedInUserUsername, profileUserId) {
+export async function isUserFollowingProfile(loggedInUserUsername: string, profileUserId: string): Promise<string> {
   const result = await firebase
     .firestore()
     .collection('users')
     .where('username', '==', loggedInUserUsername) // karl (active logged in user)
     .where('following', 'array-contains', profileUserId)
     .get();
-  const [response = {}] = result.docs.map((item) => ({
-    ...item.data(),
+  const [response] = result.docs.map((item) => ({
+    ...(item.data() as responceUserData),
     docId: item.id,
   }));
 
@@ -378,26 +389,32 @@ export async function isUserFollowingProfile(loggedInUserUsername, profileUserId
 }
 
 // フォローとフォローの解除
-export async function toggleFollow(isFollowingProfile, activeUserDocId, profileDocId, profileUserId, followingUserId) {
+export async function toggleFollow(
+  isFollowingProfile: boolean,
+  activeUserDocId: string,
+  profileDocId: string,
+  profileUserId: string,
+  followingUserId: string
+): Promise<void> {
   await updateLoggedInUserFollowing(activeUserDocId, profileUserId, isFollowingProfile);
 
   await updateFollowedUserFollowers(profileDocId, followingUserId, isFollowingProfile);
 }
 
 // フォローしている人のuserドキュメントを取得
-export async function getProfileFollowingUsers(following) {
-  let users = [];
+export async function getProfileFollowingUsers(following: string[]): Promise<responceBothData[]> {
+  let users: responceBothData[] = [];
   if (following.length > 0) {
     const collectionPath = firebase.firestore().collection('users');
-    const getQuery = (batch) => collectionPath.where('userId', 'in', batch);
+    const getQuery = (batch: string[]) => collectionPath.where('userId', 'in', batch);
     users = await getDocumentByArraysIn(following, getQuery);
   }
 
   return users;
 }
 // フォローされている人のuserドキュメントを取得
-export async function getProfileFollowedgUsers(followed) {
-  let users = [];
+export async function getProfileFollowedgUsers(followed: string[]): Promise<responceBothData[]> {
+  let users: responceBothData[] = [];
 
   if (followed.length > 0) {
     const collectionPath = firebase.firestore().collection('users');
